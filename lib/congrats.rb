b5681ac5a9192a0d2f613d6ed1d4db9260246b8e
brianjgmartin <brianjgmartin@gmail.com>
2015-03-25 10:56:41 +0000

    Change to Congrats

--- lib/congrats.rb
+++ lib/congrats.rb
@@ -1,13 +1,13 @@
 #!/home/brianmartin/.rvm/rubies/ruby-2.1.5/bin/ruby
 
 class Congratulations
-	
+
 	# Returns level results on Completion
 	def self.response(level, correct, wrong, name)
-			@congrats = "Congratulations " + name + " You have completed Level " + level.to_s
-			@right = "You got All " + correct.to_s + " Questions Correct"
-			@notcorrect = "You got " + wrong.to_s + " Wrong Answers"
-		return @congrats, @right, @notcorrect
+		@congrats = "Congratulations " + name + " You have completed Level " + level.to_s
+		@right = "You got All " + correct.to_s + " Questions Correct"
+		@notcorrect = "You got " + wrong.to_s + " Wrong Answers"
+	return @congrats, @right, @notcorrect
 	end
 end
 
